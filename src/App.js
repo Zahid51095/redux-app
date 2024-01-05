10bb1953aa5ad99745db9bc43fc329b80d3aa1dc
Zahid Hasan <zahidhasan140338@gmail.com>
2024-01-05 22:39:44 +0600

    created a counter app with redux store

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { incAction, decAction } from './redux';
 
 function App() {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => dispatch(incAction())}>Increment</button>
+      <button onClick={() => dispatch(decAction())}>Decrement</button>
     </div>
   );
 }
